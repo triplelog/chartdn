8203e08148654294dab53b599f2f7401676afe00
triplelog <robmwilc@gmail.com>
2020-01-16 15:26:54 -0500

    first commit

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -52,7 +52,41 @@ wss.on('connection', function connection(ws) {
   		console.log(dm.message)
   	}
   	else if (dm.operation == 'download'){
-  		exec('wget -O newtable.csv '+dm.message);
+  		
+
+		const request = require('request');
+		let file = fs.createWriteStream('file.csv');
+		/* Using Promises so that we can use the ASYNC AWAIT syntax */        
+		await new Promise((resolve, reject) => {
+			let stream = request({
+				/* Here you should specify the exact link to the file you are trying to download */
+				uri: dm.message,
+				headers: {
+					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+					'Accept-Encoding': 'gzip, deflate, br',
+					'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+					'Cache-Control': 'max-age=0',
+					'Connection': 'keep-alive',
+					'Upgrade-Insecure-Requests': '1',
+					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+				},
+				/* GZIP true for most of the websites now, disable it if you don't need it */
+				gzip: true
+			})
+			.pipe(file)
+			.on('finish', () => {
+				console.log(`The file is finished downloading.`);
+				resolve();
+			})
+			.on('error', (error) => {
+				reject(error);
+			})
+		})
+		.catch(error => {
+			console.log(`Something happened: ${error}`);
+		});
+
+
   	}
   });
 });
