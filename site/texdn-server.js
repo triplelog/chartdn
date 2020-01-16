7af58bed0207c631503f36730decb2abd3ea8748
triplelog <robmwilc@gmail.com>
2020-01-16 15:32:01 -0500

    first commit

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -54,31 +54,13 @@ wss.on('connection', function connection(ws) {
   	else if (dm.operation == 'download'){
   		
 
-		const request = require('request');
-		let file = fs.createWriteStream('file.csv');
-			let stream = request({
-				/* Here you should specify the exact link to the file you are trying to download */
-				uri: dm.message,
-				headers: {
-					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-					'Accept-Encoding': 'gzip, deflate, br',
-					'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
-					'Cache-Control': 'max-age=0',
-					'Connection': 'keep-alive',
-					'Upgrade-Insecure-Requests': '1',
-					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-				},
-				/* GZIP true for most of the websites now, disable it if you don't need it */
-				gzip: true
-			})
-			.pipe(file)
-			.on('finish', () => {
-				console.log(`The file is finished downloading.`);
-				resolve();
-			})
-			.on('error', (error) => {
-				reject(error);
-			})
+		var file = fs.createWriteStream('file.csv');
+		  var request = https.get(dm.message, function(response) {
+			response.pipe(file);
+			file.on('finish', function() {
+			  file.close();
+			});
+		  });
 
 
   	}
