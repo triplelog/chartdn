ca07e4c800632432271f93919e2ae3154926d984
triplelog <robmwilc@gmail.com>
2020-01-28 19:03:25 -0500

    first commit

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -15,13 +15,8 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/chartdn.com/fullchain.pem')
 };
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
-const User = new Schema({});
-User.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', User);
 
+const User = require('./models/user');
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 // use static authenticate method of model in LocalStrategy
