a5241c964ed9f5d8e784a049cb31dcc458671bc3
triplelog <robmwilc@gmail.com>
2020-01-31 11:55:07 -0500

    first commit

--- site/static/js/account.js
+++ site/static/js/account.js
@@ -1,11 +1,14 @@
 hideAll();
+document.getElementById('tabcreated').classList.add('pure-menu-selected');
 function hideAll() {
-	document.getElementById('created').style.display = 'none';
-	document.getElementById('favorites').style.display = 'none';
-	document.getElementById('friends').style.display = 'none';
-	document.getElementById('settings').style.display = 'none';
+	var tabids = ['created','favorites','friends','settings'];
+	for (var i=0;i<tabids.length;i++){
+		document.getElementById(tabids[i]).style.display = 'none';
+		document.getElementById('tab'+tabids[i]).classList.remove('pure-menu-selected');
+	}
 }
 function chgTab(tabid){
 	hideAll();
 	document.getElementById(tabid).style.display = 'block';
+	document.getElementById('tab'+tabid).classList.add('pure-menu-selected');
 }
