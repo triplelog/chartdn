f4c6a7d8cde6647fdec3b5ff71ba5c91fb91f488
triplelog <robmwilc@gmail.com>
2020-01-30 22:48:35 -0500

    first commit

--- site/python/createrobo.py
+++ site/python/createrobo.py
@@ -3,6 +3,18 @@ import sys
 
 hash = sys.argv[1]
 rh = Robohash(hash)
-rh.assemble(roboset='any')
-with open("static/robots/"+hash+".png", "wb") as f:
+rh.assemble(roboset='set1')
+with open("static/robots/"+hash+"1.png", "wb") as f:
+    rh.img.save(f, format="png")
+rh.assemble(roboset='set2')
+with open("static/robots/"+hash+"2.png", "wb") as f:
+    rh.img.save(f, format="png")
+rh.assemble(roboset='set3')
+with open("static/robots/"+hash+"3.png", "wb") as f:
+    rh.img.save(f, format="png")
+rh.assemble(roboset='set4')
+with open("static/robots/"+hash+"4.png", "wb") as f:
+    rh.img.save(f, format="png")
+rh.assemble(roboset='set5')
+with open("static/robots/"+hash+"5.png", "wb") as f:
     rh.img.save(f, format="png")
