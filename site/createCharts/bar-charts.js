6676773871a6b0f0272bfb48e77a9f078d891f7f
triplelog <robmwilc@gmail.com>
2020-02-05 12:01:22 -0500

    first commit

--- site/createCharts/bar-charts.js
+++ site/createCharts/bar-charts.js
@@ -1,26 +1,4 @@
-exports.createPlotly = function(data,options) {
-	var datasets = [];
-	if (!options['yColumns'] || options['yColumns'].length == 0){return {};}
-	for (var i=0;i<options['yColumns'].length;i++){
-		if (options.xColumn != ''){
-			datasets.push({'x':data['bycol'][options['xColumn']], 'y':data['bycol'][options['yColumns'][i]], 'type': 'bar'});
-		}
-		else {
-			datasets.push({'x':i, 'y':data['bycol'][options['yColumns'][i]], 'type': 'bar'});
 
-		}
-	}
-	
-	chartOptions = {}
-	if (options.title) {chartOptions['title']=options.title;}
-	if (options.stepSizeX != '' && options.stepSizeX != 'default') {chartOptions['xaxis']={'dtick':options.stepSizeX};}
-	if (options.stepSizeY != '' && options.stepSizeY != 'default') {chartOptions['yaxis']={'dtick':options.stepSizeY};}
-
-	
-	var chartJSON = {'data':datasets,'options':chartOptions}
-	return chartJSON;
-
-}
 
 
 
