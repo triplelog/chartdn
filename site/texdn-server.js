35159b08ddfe87594cabc1c9b1f2ed4c0e0b518c
triplelog <robmwilc@gmail.com>
2020-02-08 14:47:13 -0500

    first commit

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -108,8 +108,6 @@ function updateOptions(oldOptions, newOptions) {
 		}
 		else if (v && typeof v === 'object' && v.constructor === Object){
 			if (!oldOptions[k]){oldOptions[k]={};}
-			oldOptions[k]={};
-			console.log(k,v);
 			for (var key in v){
 				oldOptions[k][key] = v[key];
 			}
