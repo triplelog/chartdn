505a622f1e568833e34ab9f6efe8ff6a59eae8fc
triplelog <robmwilc@gmail.com>
2020-02-08 22:15:21 -0500

    first commit

--- site/speed-test.js
+++ site/speed-test.js
@@ -1,2 +1,6 @@
-for (var i=0;i<1000000;i++) {var xxx = Math.random()+i;};
-console.log('done');
+var fib = function(n) {
+  if (n <= 1) { return 1; }
+  return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(46));
