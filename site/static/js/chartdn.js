1de3ef2b79da1509702106228c32f37d7e8f32ef
triplelog <robmwilc@gmail.com>
2020-02-12 21:50:48 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -658,12 +658,14 @@ function updateModifier(evt){
 					var ellc = ell.children;
 					var elExists = false;
 					console.log(ellc);
-					for (var ii in ellc){
-						console.log(ii, ellc[ii]);
-						if (ellc[ii].getAttribute('name') == name){
-							ellc[ii].textContent = name + ' := ' + type + ' of ' + col;
-							elExists = true;
-							break;
+					if (ellc.length > 0){
+						for (var ii in ellc){
+							console.log(ii, ellc[ii]);
+							if (ellc[ii].getAttribute('name') == name){
+								ellc[ii].textContent = name + ' := ' + type + ' of ' + col;
+								elExists = true;
+								break;
+							}
 						}
 					}
 					if (!elExists){
