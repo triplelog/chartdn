27a5e4ca6cfcb08ece2f9b41e8e1ac20c5e2ec71
triplelog <robmwilc@gmail.com>
2020-02-14 15:39:40 -0500

    first

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -647,6 +647,7 @@ function convertDataToFull(dataStr,nHeaders,modifiers,nsteps) {
 function makeAllCharts(ws,dm,chartInfo,chartStyle='all') {
 	var t0 = performance.now();
 	fs.readFile('saved/'+chartInfo.data, 'utf8', function(err, fileData) {
+		if (err){console.log(err);}
 		console.log(chartInfo.data,fileData);
 		var results = Papa.parse(fileData, {
 			//delimiter: chartInfo.options.delimiter || "",
