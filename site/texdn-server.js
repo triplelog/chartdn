081a8b741caf0aa1d47a6859320699ffd29d8e52
triplelog <robmwilc@gmail.com>
2020-02-14 12:37:56 -0500

    first

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -15,6 +15,7 @@ var nunjucks = require('nunjucks');
 var crypto = require("crypto");
 const pako = require('pako');
 const atob = require('atob');
+const btoa = require('btoa');
 var createPlotly = require('./createCharts/plotly-charts.js');
 var createXkcd = require('./createCharts/xkcd-charts.js');
 var createGoogle = require('./createCharts/google-charts.js');
