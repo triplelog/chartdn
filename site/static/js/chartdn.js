b22d96720cb2e65e557c62b2e52a2b7eba4a7126
triplelog <robmwilc@gmail.com>
2020-02-18 18:10:16 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -12,7 +12,7 @@ ws.onopen = function(evt) {
 	ws.send(JSON.stringify(jsonmessage));
 }
 ws.onmessage = function(evt){
-	var d = new Date(); var n = d.getTime(); console.log('time6: ', n);
+	//var d = new Date(); var n = d.getTime(); console.log('time6: ', n);
 	var dm = JSON.parse(evt.data);
 	if (dm.operation == 'downloaded'){
 		document.getElementById('dataCopy').value = dm.message;
@@ -22,7 +22,7 @@ ws.onmessage = function(evt){
 		chartid = dm.message;
 	}
 	else if (dm.operation == 'chart'){
-		var d = new Date(); var n = d.getTime(); console.log('time7: ', n);
+		//var d = new Date(); var n = d.getTime(); console.log('time7: ', n);
 		var chartJSON = dm.message;
 		var allCharts = document.querySelectorAll('chartdn-chart');
 		for (var i=0;i< allCharts.length;i++){
@@ -30,9 +30,9 @@ ws.onmessage = function(evt){
 				allCharts[i].makeChart(chartJSON);
 			}
 		}
-		var d = new Date(); var n = d.getTime(); console.log('time8: ', n);
+		//var d = new Date(); var n = d.getTime(); console.log('time8: ', n);
 		updateTable(dm.mdata);
-		var d = new Date(); var n = d.getTime(); console.log('time9: ', n);
+		//var d = new Date(); var n = d.getTime(); console.log('time9: ', n);
 		allHeaders = dm.allHeaders;
 		updateColumns();
 	}
