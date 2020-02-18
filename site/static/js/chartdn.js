f9059f1690370403e5b9ff09fdb8e2df3eb781cf
triplelog <robmwilc@gmail.com>
2020-02-18 18:39:50 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -705,7 +705,7 @@ function updateTable(data,idx=0) {
 			thisColumn.title = 'Row';
 			thisColumn.field = 'colRow';
 			thisColumn.cellClick = function(e, cell){
-				console.log(cell.getRow());
+				console.log(cell.getRow()['_row']);
 			}
 			tableColumns[0] = thisColumn;
 		}
