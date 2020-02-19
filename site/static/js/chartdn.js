077cc525cf5685f69fba5b51db8836c4a471b314
triplelog <robmwilc@gmail.com>
2020-02-19 18:14:47 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -769,7 +769,7 @@ function updateTable(data) {
 			//row - row component
 			var cells = row.getCells();
 			for (var cell in cell){
-				cell.style.height = "1.5rem";
+				//cell.style.height = "1.5rem";
 			}
 			row.getElement().style.height = "1.5rem";
 		},
