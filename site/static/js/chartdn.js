36895945339eff50743e145caca3d157056c70c2
triplelog <robmwilc@gmail.com>
2020-02-19 17:08:07 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -754,8 +754,8 @@ function updateTable(data) {
 		renderComplete:function(){
 			if (this.tableWidth && this.options.layout == 'fitData'){
 				var el = document.querySelector('div.tabulator-tableHolder');
-				console.log(el.offsetWidth,el.clientWidth);
-				var nWidth = this.tableWidth + 20;
+				console.log(el.offsetWidth,el.clientWidth,el.innerWidth);
+				var nWidth = this.tableWidth + 18;
 				document.getElementById("dataTableModified").style.width = nWidth+'px';
 			}
     	},
