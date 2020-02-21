178755ad3355024b01644ad2a708f508268e5e27
triplelog <robmwilc@gmail.com>
2020-02-20 23:11:28 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -379,7 +379,7 @@ function columnsChg() {
 				noNames = true;
 			}
 			if (!nsteps && nsteps !=0) {
-				var qstring = 'div[tabulator-field="col'+parseInt(yCols[i])+'"] div.tabulator-colo-title';
+				var qstring = 'div[tabulator-field="col'+parseInt(yCols[i])+'"] div.tabulator-col-title';
 				console.log(qstring);
 				var hEl = headerEls.querySelector(qstring);
 				console.log(hEl);
