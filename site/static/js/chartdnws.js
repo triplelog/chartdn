6c68eaee180eb63b4cc11fd984dbc71d12a2076e
triplelog <robmwilc@gmail.com>
2020-02-20 20:13:17 -0500

    first

--- site/static/js/chartdnws.js
+++ site/static/js/chartdnws.js
@@ -2,7 +2,7 @@ var ws = new WebSocket('wss://chartdn.com:8080');
 ws.onopen = function(evt) {
 	var allcharts = document.querySelectorAll('chartdn-chart');
 	for (var i=0;i<allcharts.length;i++){
-		allcharts[i].setAttribute('data-loc') = i;
+		allcharts[i].setAttribute('data-loc',i);
 		var jsonmessage = {'operation':'view','id':allcharts[i].getAttribute('src'),'loc':i,'style':allcharts[i].getAttribute('data-style')}
 		ws.send(JSON.stringify(jsonmessage));
 	}
