f3867fda56534825edf51cb50313c37f50887c04
triplelog <robmwilc@gmail.com>
2020-02-21 09:03:36 -0500

    first

--- site/static/js/account.js
+++ site/static/js/account.js
@@ -23,6 +23,11 @@ function chgChart(type,chg){
 		var el = document.getElementById(type+'Chart');
 		el.setAttribute('src',charts[type][n[type]]);
 	}
+	else if (chg == -2){
+		n[type]=0;
+		var el = document.getElementById(type+'Chart');
+		el.setAttribute('src',charts[type][n[type]]);
+	}
 	else if (chg == 1){
 		n[type]++;
 		if (n[type]>charts[type].length-1){
@@ -31,4 +36,59 @@ function chgChart(type,chg){
 		var el = document.getElementById(type+'Chart');
 		el.setAttribute('src',charts[type][n[type]]);
 	}
+	else if (chg == 2){
+		n[type] = charts[type].length-1;
+		var el = document.getElementById(type+'Chart');
+		el.setAttribute('src',charts[type][n[type]]);
+	}
 }
+minimizedBoxes = {};
+minimizedBoxes.created = 'half';
+function minimizeBox(type,full=false){
+	var el = document.getElementById(type+'Box');
+	if (full){
+		el.classList.add('pure-u-1-1');
+		el.classList.remove('pure-u-1-2');
+		el.style.display = 'block';
+		minimizedBoxes[type] = 'full';
+		var myStyle = el.querySelector('chartdn-chart').getAttribute('data-style');
+		var loc = el.querySelector('chartdn-chart').getAttribute('data-loc');
+		var jsonmessage = {'operation':'view','id':chartid,'loc':loc,'style':myStyle};
+		ws.send(JSON.stringify(jsonmessage));
+		var el2 = document.getElementById(type+'None');
+		el2.style.display = 'none';
+	}
+	else if (minimizedBoxes[type] == 'full') {
+		el.classList.add('pure-u-1-2');
+		el.classList.remove('pure-u-1-1');
+		el.style.display = 'block';
+		var myStyle = el.querySelector('chartdn-chart').getAttribute('data-style');
+		var loc = el.querySelector('chartdn-chart').getAttribute('data-loc');
+		var jsonmessage = {'operation':'view','id':chartid,'loc':loc,'style':myStyle}
+		ws.send(JSON.stringify(jsonmessage));
+		minimizedBoxes[type] = 'half';
+	}
+	else if (minimizedBoxes[type] == 'half') {
+		el.style.display = 'none';
+		var el2 = document.getElementById(type+'None');
+		el2.style.display = 'block';
+		minimizedBoxes[type] = 'none';
+	}
+	else if (minimizedBoxes[type] == 'none') {
+		el.classList.add('pure-u-1-2');
+		el.classList.remove('pure-u-1-1');
+		el.style.display = 'block';
+		var myStyle = el.querySelector('chartdn-chart').getAttribute('data-style');
+		var loc = el.querySelector('chartdn-chart').getAttribute('data-loc');
+		var jsonmessage = {'operation':'view','id':chartid,'loc':loc,'style':myStyle}
+		ws.send(JSON.stringify(jsonmessage));
+		minimizedBoxes[type] = 'half';
+		var el2 = document.getElementById(type+'None');
+		el2.style.display = 'none';
+	}
+	
+}
+
+
+
+
