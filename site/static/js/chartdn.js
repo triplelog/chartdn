776d787a6e0a79386ede7467da66b087273faf66
triplelog <robmwilc@gmail.com>
2020-02-21 20:18:15 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -683,6 +683,8 @@ function clickTippy(evt) {
 			var newColumn = {rowHandle:true, formatter:"handle", headerSort:false, frozen:true, width:30, minWidth:30};
 			table.options.columns[0] = newColumn;
 			redrawTable();
+			console.log(table.options.columns);
+			redrawTable();
 			tippysR[row].destroy();
 			delete tippysR[row];
 		}
