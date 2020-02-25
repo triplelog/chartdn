8c156a252e3067dc86f6fa1ffd4120fdf6899cf6
triplelog <robmwilc@gmail.com>
2020-02-24 22:54:55 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -781,7 +781,7 @@ function updateTable(data,sentHeaders) {
 			  trigger: 'manual',
 			  interactive: true,
 			  placement: 'bottom',
-			  onHide(instance){this.destroy(); delete tippys[col];}
+			  onHide(instance){instance.destroy(); delete tippys[col];}
 			});
 			tippys[col] = mytippy;
 			mytippy.show();
