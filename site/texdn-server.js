75afaed6f92e0f24c30fe9a92c68ae03921fe219
triplelog <robmwilc@gmail.com>
2020-02-25 14:29:36 -0500

    first

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -560,7 +560,10 @@ wss.on('connection', function connection(ws) {
 			  	}
 			  	else if (result.users.view[0] == 'friends' && username != ''){
 			  		//Check if result.users.creator is a friend
-			  		console.log(result.users, username)
+			  		console.log(result.users, username);
+			  			User.findOne({username: username}, function(err,result){
+			  				console.log(result);
+			  			})
 			  		 User.countDocuments({username: username, followers: result.users.creator}, function(err, result2) {
 						if (err){return}
 						else if (result2 > 0){
