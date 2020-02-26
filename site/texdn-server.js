9e9b68add642e0617356c9ae82e8c45237ef3c99
triplelog <robmwilc@gmail.com>
2020-02-26 00:10:12 -0500

    first

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -556,8 +556,10 @@ wss.on('connection', function connection(ws) {
   	else if (dm.operation == 'view'){
 		  chartid = dm.id;
 		  if (chartid && chartid != ""){
+		  	  console.log(chartid);
 			  Chart.findOne({ id: chartid }, function(err, result) {
 			  	if (err || result == null){
+			  		console.log(err);
 			  	}
 			  	else if (result.users.view[0]=='any' || result.users.creator == username){
 			  		console.log('viewable');
