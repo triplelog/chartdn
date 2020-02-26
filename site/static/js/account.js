a4c977df01a99475ed3f0cc47e6c6f6295d966f2
triplelog <robmwilc@gmail.com>
2020-02-26 12:32:52 -0500

    first

--- site/static/js/account.js
+++ site/static/js/account.js
@@ -45,12 +45,14 @@ ws.onmessage = function(evt){
 		}
 	}
 	else if (dm.operation == 'friend'){
-		var newDiv = document.createElement('div');
-		var newA = document.createElement('a');
-		newA.setAttribute('href','../user/'+dm.message);
-		newA.textContent = dm.message;
-		newDiv.appendChild(newA);
-		document.getElementById('friendList').appendChild(newDiv);
+		if (document.getElementById('friendList')) {
+			var newDiv = document.createElement('div');
+			var newA = document.createElement('a');
+			newA.setAttribute('href','../user/'+dm.message);
+			newA.textContent = dm.message;
+			newDiv.appendChild(newA);
+			document.getElementById('friendList').appendChild(newDiv);
+		}
 	}
 	else if (dm.operation == 'result'){
 		var type = dm.key
