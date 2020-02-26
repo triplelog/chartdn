25ef619e5797a7d8813040d4944b16a769d8389b
triplelog <robmwilc@gmail.com>
2020-02-26 09:48:42 -0500

    first

--- site/login-server.js
+++ site/login-server.js
@@ -203,17 +203,16 @@ app2.post('/register',
 function usernameToLowerCase(req, res){
 	req.body.username = req.body.username.toLowerCase();
 } 
-app2.post('/login', 
-	router.post("/login", usernameToLowerCase,
+app2.post('/login',  usernameToLowerCase,
 	passport.authenticate('local', { successRedirect: '/account', failureRedirect: '/fail' }),
 	function (req, res) {}
-	);
+);
   /*function(req, res){
   	  console.log(req);
 	  req.body.username = req.body.username.toLowerCase();
 	  passport.authenticate('local', { successRedirect: '/account', failureRedirect: '/fail' })
-  }*/
-);
+  }
+);*/
 
 
 
