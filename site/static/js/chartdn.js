2b6278d9b140af6c680830a8e1aebfa8d2b68601
triplelog <robmwilc@gmail.com>
2020-02-25 19:37:58 -0500

    first

--- site/static/js/chartdn.js
+++ site/static/js/chartdn.js
@@ -729,6 +729,8 @@ function updateTable(data,sentHeaders) {
 		tcr.setAttribute('data-row',row);
 		
 		if (!modifiers || modifiers.length == 0 || nsteps == 0) {
+			tcr.querySelector('button[name=deleteButton]').style.display = 'block';
+			tcr.querySelector('button[name=addButton]').style.display = 'block';
 			tcr.querySelector('button[name=deleteButton]').addEventListener('click',clickTippy);
 			tcr.querySelector('button[name=addButton]').addEventListener('click',clickTippy);
 		}
@@ -737,7 +739,9 @@ function updateTable(data,sentHeaders) {
 			tcr.querySelector('button[name=addButton]').style.display = 'none';
 		}
 		if (!modifiers || modifiers.length == 0 || (!nsteps && nsteps != 0) ){
+			tcr.querySelector('button[name=filterButton]').style.display = 'block';
 			tcr.querySelector('button[name=filterButton]').addEventListener('click',clickTippy);
+			
 		}
 		else {
 			tcr.querySelector('button[name=filterButton]').style.display = 'none';
