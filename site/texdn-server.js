04b8bd566bc6a7bf0cb3f70ae579c7fa1043d1b9
triplelog <robmwilc@gmail.com>
2020-02-27 00:09:51 -0500

    more

--- site/texdn-server.js
+++ site/texdn-server.js
@@ -591,7 +591,7 @@ wss.on('connection', function connection(ws) {
 		  }
   	}
   	else if (dm.operation == 'qr'){
-  		var dotnet = 'dotnet python/qr-art/src/qr-art/bin/Debug/netcoreapp3.1/qr-art.dll "https://chartdn.com/charts/yoigjmppb" static/images/favicon.png png static/images/testnew.png -t 8 -e H';
+  		var dotnet = 'dotnet python/qr-art/src/qr-art/bin/Debug/netcoreapp3.1/qr-art.dll "https://chartdn.com/charts/yoigjmppb" static/images/favicon.png png static/images/testnew.png -t 7 -e H';
 		console.log('making qr', performance.now());
 		var child = exec(dotnet, function(err, stdout, stderr) {
 			if (err) throw err;
