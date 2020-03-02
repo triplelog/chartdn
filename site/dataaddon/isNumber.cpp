81750dfbb4a7008217e5739db8801c188e933d5b
triplelog <robmwilc@gmail.com>
2020-03-02 16:47:46 -0500

    new

---
+++ site/dataaddon/isNumber.cpp
@@ -0,0 +1,14 @@
+#include <nan.h>
+#include <string>
+
+
+bool isNumber(input_str) {
+	int sz = input_str.size();
+	int i=0;
+	for (i=0;i<sz;i++){
+		if (input_str[i] == '9'){
+			return true;
+		}
+	}
+	return false;
+}
