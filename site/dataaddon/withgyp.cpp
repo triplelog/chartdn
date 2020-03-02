17d88c033b71adcd62971f88ea00334c0fd365e5
triplelog <robmwilc@gmail.com>
2020-03-02 14:51:06 -0500

    new

--- site/dataaddon/withgyp.cpp
+++ site/dataaddon/withgyp.cpp
@@ -1,9 +1,27 @@
 #include <nan.h>
+#include "sync.h"   // NOLINT(build/include)
+#include "async.h"  // NOLINT(build/include)
 
-// Simple synchronous access to the `Estimate()` function
+using v8::FunctionTemplate;
+using v8::Handle;
+using v8::Object;
+using v8::String;
+using Nan::GetFunction;
+using Nan::New;
+using Nan::Set;
+
 NAN_METHOD(CalculateSync) {
   // expect a number as the first argument
   int points = info[0]->Uint32Value();
 
   info.GetReturnValue().Set(points);
 }
+
+// Estimate() function
+NAN_MODULE_INIT(InitAll) {
+  Set(target, New<String>("calculateSync").ToLocalChecked(),
+    GetFunction(New<FunctionTemplate>(CalculateSync)).ToLocalChecked());
+}
+
+NODE_MODULE(addon, InitAll)
+
