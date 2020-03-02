45c57065ee31571d0c273f7a11c2329d48f97e08
triplelog <robmwilc@gmail.com>
2020-03-02 15:53:32 -0500

    new

--- site/dataaddon/withgyp.cpp
+++ site/dataaddon/withgyp.cpp
@@ -5,7 +5,7 @@
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
 	v8::Local<v8::String> xx = info[0]->ToString(context).ToLocalChecked();
-  	info.GetReturnValue().Set(Nan::New("hello").ToLocalChecked());
+  	info.GetReturnValue().Set(xx);
 }
 
 void Init(v8::Local<v8::Object> exports) {
