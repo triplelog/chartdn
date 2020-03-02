757cb9642297a0bffa4fe693830842ea9a625215
triplelog <robmwilc@gmail.com>
2020-03-02 16:29:16 -0500

    new

--- site/dataaddon/withgyp.cpp
+++ site/dataaddon/withgyp.cpp
@@ -4,11 +4,11 @@
 
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	v8::Isolate* isolate = info.GetIsolate();
-	v8::String::Utf8Value s(isolate, info[0]).ToLocalChecked();
+	v8::String::Utf8Value s(isolate, info[0]);
     std::string str(*s);
     std::reverse(str.begin(), str.end());    
 
-    v8::Local<v8::String> retval = v8::String::NewFromUtf8(isolate, str.c_str());
+    v8::Local<v8::String> retval = v8::String::NewFromUtf8(isolate, str.c_str()).ToLocalChecked();
     //args.GetReturnValue().Set(retval);
 	//v8::Local<v8::String> xxx = xx + xx;
   	//info.GetReturnValue().Set(xxx);
