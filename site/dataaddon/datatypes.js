33e773a6a46df0c0d43f5d382113e764a410fd40
triplelog <robmwilc@gmail.com>
2020-03-02 11:00:54 -0500

    new

--- site/dataaddon/datatypes.js
+++ site/dataaddon/datatypes.js
@@ -1,6 +1,6 @@
 'use strict';
 const assert = require('assert');
-const bindingPath = require.resolve(`./build/datatypes`);
+const bindingPath = require.resolve(`./build/binding`);
 const datatypes = require(bindingPath);
 assert.strictEqual(datatypes.hello(), 'world');
-console.log('datatypes.hello() =', binding.hello());
+console.log('datatypes.hello() =', datatypes.hello());
