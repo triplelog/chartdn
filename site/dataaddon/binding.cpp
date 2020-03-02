2c495e3495394686fe08bbf7f06d6da3a73139a1
triplelog <robmwilc@gmail.com>
2020-03-02 12:15:43 -0500

    new

--- site/dataaddon/binding.cpp
+++ site/dataaddon/binding.cpp
@@ -44,4 +44,11 @@ Napi::Object functionexample::Init(Napi::Env env, Napi::Object exports)
   return exports;
 }
 
+Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+	return exports;
+	return functionexample::Init(env, exports);
+ }
+ 
+NODE_API_MODULE(testaddon, InitAll)
+
 
