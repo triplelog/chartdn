8663fb70ea3c7b87491887fc3256a66b3f842398
triplelog <robmwilc@gmail.com>
2020-03-02 12:26:43 -0500

    new

--- site/dataaddon/datatypes.js
+++ site/dataaddon/datatypes.js
@@ -1,5 +1,14 @@
 'use strict';
+const { PerformanceObserver, performance } = require('perf_hooks');
+
 const assert = require('assert');
 const bindingPath = require.resolve(`./build/Release/binding`);
 const datatypes = require(bindingPath);
-console.log(datatypes.getType('a'));
+
+var allouts = [];
+console.log(performance.now());
+for (var i=0;i<1000;i++){
+	var out = datatypes.getType('a'+i);
+	allouts.push(out);
+}
+console.log(performance.now());
