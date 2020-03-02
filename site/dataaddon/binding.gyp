3e51b9261521a9f379e939936a3962a3e5c98ccd
triplelog <robmwilc@gmail.com>
2020-03-02 10:59:17 -0500

    new

--- site/dataaddon/binding.gyp
+++ site/dataaddon/binding.gyp
@@ -3,7 +3,6 @@
     {
       'target_name': 'datatypes',
       'sources': [ 'datatypes.cc' ],
-      'includes': ['../common.gypi'],
     }
   ]
 }
--- site/dataaddon/datatypes.js
+++ site/dataaddon/datatypes.js
@@ -1,7 +1,6 @@
 'use strict';
-const common = require('../../common');
 const assert = require('assert');
-const bindingPath = require.resolve(`./build/${common.buildType}/datatypes`);
+const bindingPath = require.resolve(`./build/datatypes`);
 const datatypes = require(bindingPath);
 assert.strictEqual(datatypes.hello(), 'world');
 console.log('datatypes.hello() =', binding.hello());
