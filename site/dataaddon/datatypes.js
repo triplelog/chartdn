6982e87a11c6e1e011770aea320d45f7b0988aee
triplelog <robmwilc@gmail.com>
2020-03-02 22:26:57 -0500

    new

--- site/dataaddon/datatypes.js
+++ site/dataaddon/datatypes.js
@@ -7,7 +7,7 @@ const datatypes = require(bindingPath);
 
 
 function getDataType(x) {
-	var t = datatypes.hello(x)[0]];
+	var t = datatypes.hello(x)[0];
 	return t;
 }
 exports.makeTypes = function(data){
