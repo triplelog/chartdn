67bbbdaabd7f7487a8fe3d58a6203bd682c292e2
triplelog <robmwilc@gmail.com>
2020-03-02 21:51:00 -0500

    new

--- site/dataaddon/datatypes.cpp
+++ site/dataaddon/datatypes.cpp
@@ -27,8 +27,8 @@ void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     v8::Local<v8::String> w = v8::String::NewFromUtf8(isolate, "w").ToLocalChecked();
     v8::Local<v8::String> t = v8::String::NewFromUtf8(isolate, "t").ToLocalChecked();
 	retobj->Set(isolate.GetContext(),v,v8::Number::New(isolate,x.v));
-	retobj->Set(isolate,w,v8::Number::New(isolate,x.w));
-	retobj->Set(isolate,t,v8::String::NewFromUtf8(isolate,&x.t);
+	retobj->Set(isolate.GetContext(),w,v8::Number::New(isolate,x.w));
+	retobj->Set(isolate.GetContext(),t,v8::String::NewFromUtf8(isolate,&x.t);
 	info.GetReturnValue().Set(retobj);
 	
     
