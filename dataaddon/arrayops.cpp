e4503561196d723767158c84d613b78dcfc829b7
triplelog <robmwilc@gmail.com>
2020-03-04 10:18:06 -0500

    new

--- dataaddon/arrayops.cpp
+++ dataaddon/arrayops.cpp
@@ -58,12 +58,14 @@ void MethodCol(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	
 	int ii=0;
 	for (ii=0;ii<sz;ii++){
-		const char* t = &temparray[ii][col].t;
-		Nan::MaybeLocal<v8::String> tt = Nan::New<v8::String>(t, 1);
-		Nan::Set(outArray,ii*3+0,tt.ToLocalChecked());
-		Nan::Set(outArray,ii*3+1,v8::Number::New(isolate,temparray[ii][col].v));
-		Nan::Set(outArray,ii*3+2,v8::Number::New(isolate,temparray[ii][col].w));
-		//Nan::Set(outArray,ii,v8::String::NewFromUtf8(isolate,&statrow[ii].t).ToLocalChecked());
+		if (col < temparray[ii].size()){
+			const char* t = &temparray[ii][col].t;
+			Nan::MaybeLocal<v8::String> tt = Nan::New<v8::String>(t, 1);
+			Nan::Set(outArray,ii*3+0,tt.ToLocalChecked());
+			Nan::Set(outArray,ii*3+1,v8::Number::New(isolate,temparray[ii][col].v));
+			Nan::Set(outArray,ii*3+2,v8::Number::New(isolate,temparray[ii][col].w));
+			//Nan::Set(outArray,ii,v8::String::NewFromUtf8(isolate,&statrow[ii].t).ToLocalChecked());
+		}
 	}
 	info.GetReturnValue().Set(outArray);
 }
