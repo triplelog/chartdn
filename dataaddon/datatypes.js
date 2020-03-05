e61ea8d185f7278076e7e110d75ea5e0d1469d38
triplelog <robmwilc@gmail.com>
2020-03-04 19:52:21 -0500

    new

--- dataaddon/datatypes.js
+++ dataaddon/datatypes.js
@@ -176,7 +176,7 @@ exports.newCol = function(options){
 	//var fullmap = makeFullMap(options);
 	var intstr = ["a","b","1"];
 	var exp = "##+#+";
-	vars = ["max","a",1,0,-1,-2,-2,"value","b",2,-2,-2,0,0];
+	var vars = ["max","a",1,0,-1,-2,-2,"value","b",2,-2,-2,0,0];
 	var out = dataArray.newcolumn(intstr,exp,vars);
 	console.log(out);
 	/*for (var i in array){
